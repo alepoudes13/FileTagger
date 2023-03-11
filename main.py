9fc6d344e3ac34c17f2e3920e24ce6ffba9b10e3
Ales <alepoudes13@mail.ru>
2023-03-11 18:15:20 +0300

    Added search bar and video files support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, glob
 from tkinter import *
 from tkinter import filedialog
+from video import VideoPlayer
 from PIL import Image, ImageTk
 
 class Window:
@@ -17,6 +18,17 @@ class Window:
         self.listFrame = Frame(self.window, width = 500, height = self.window.winfo_height() - self.button_explore.winfo_height())
         self.thumbFrame = Frame(self.window, width = 500, height = self.window.winfo_height() - self.button_explore.winfo_height())
         self.window.bind('<Configure>', self.frameResize)
+        self.searchEntry = Entry(self.window, background='#cccccc')
+        self.searchEntry.grid(row = 1, column = 3)
+        self.searchEntry.bind('<KeyRelease>', self.getEntry)
+
+    def getEntry(self, event = None):
+        self.searchFilter = self.searchEntry.get()
+        self.the_listbox.delete(0, END)
+        for file in self.files:
+            if self.searchFilter in file:
+                self.the_listbox.insert(END, file)
+        
 
     def openFolder(self):
         self.dir = filedialog.askdirectory(title = "Select folder to open")
@@ -27,25 +39,33 @@ class Window:
         self.thumbFrame.config(height=self.window.winfo_height() - self.button_explore.winfo_height(), width = self.window.winfo_width() / 2)
         self.thumbFrame.place(relx=0.5, y = self.button_explore.winfo_height())
 
-    def onItemSelection(self, event):
+    def onItemSelection(self, event = None):
         selection = self.the_listbox.curselection()[0]
-        # TODO, change w,h formula
-        w, h = (self.listFrame.winfo_width(), self.listFrame.winfo_height())
-        thumb = Image.open(self.dir + '/' + self.the_listbox.get(selection))
-        if thumb.height > thumb.width:
-            w /= thumb.height / thumb.width
-        else:
-            h /= thumb.width / thumb.height
-        thumb = thumb.resize((int(w), int(h)))
-        tk_thumb = ImageTk.PhotoImage(thumb)
-        for widgets in self.thumbFrame.winfo_children():
-            widgets.destroy()
-        label = Label(self.thumbFrame, image=tk_thumb)
-        label.image = tk_thumb
-        label.place(relx=0.5, rely=0.5, anchor=CENTER)
+        w, h = (self.window.winfo_width() - self.listFrame.winfo_width(), self.listFrame.winfo_height())
+        try:
+            thumb = Image.open(self.dir + '/' + self.the_listbox.get(selection))
+            if thumb.height > thumb.width:
+                w /= thumb.height / thumb.width
+            else:
+                h /= thumb.width / thumb.height
+            thumb = thumb.resize((int(w), int(h)))
+            tk_thumb = ImageTk.PhotoImage(thumb)
+            for widgets in self.thumbFrame.winfo_children():
+                widgets.destroy()
+            label = Label(self.thumbFrame, image=tk_thumb)
+            label.image = tk_thumb
+            label.place(relx=0.5, rely=0.5, anchor=CENTER)
+        except:
+            for widgets in self.thumbFrame.winfo_children():
+                widgets.destroy()
+            video = VideoPlayer(self.thumbFrame, self.dir + '/' + self.the_listbox.get(selection), w, h)
+            #video = TkinterVideo(self.thumbFrame, keep_aspect=True)
+            #video.load(self.dir + '/' + self.the_listbox.get(selection))
+            #video.place(relx=0, rely=0, relwidth=1, relheight=1)
+            #video.play()
 
 
-    def listFilesInFolder(self, event = None):
+    def listFilesInFolder(self):
         self.frameResize()
         self.the_listbox = Listbox(self.listFrame, selectbackground="#F24FBF", font=("Calibri", "10"), background="white")
         the_scrollbar = Scrollbar(self.the_listbox,orient=VERTICAL,command=self.the_listbox.yview)
@@ -59,9 +79,9 @@ class Window:
         files = list(filter(os.path.isfile, glob.glob(self.dir + "\*")))
         files.sort(key=os.path.getctime)
         files.reverse()
-        files = [file.split("\\")[-1] for file in files]
+        self.files = [file.split("\\")[-1] for file in files]
 #        files = os.listdir(os.path.abspath(self.dir))
-        for file in files:
+        for file in self.files:
             self.the_listbox.insert(END, file)
         self.the_listbox.insert(END, "")
         self.the_listbox.insert(END, "Total Files: " + str(len(files)))
---
+++ video.py
@@ -0,0 +1,88 @@
+import datetime
+import tkinter as tk
+from tkinter import filedialog
+from tkVideoPlayer import TkinterVideo
+
+
+class VideoPlayer:
+    def __init__(self, frame: tk.Frame, path, width, height):
+        """ init player on a frame """
+        vid_frame = tk.Frame(frame, width=width, height=height - 65)
+        self.vid_player = TkinterVideo(vid_frame, keep_aspect=True)
+        self.vid_player.place(relx=0, rely=0, relwidth=1, relheight=1)
+        vid_frame.pack()
+
+        self.play_pause_btn = tk.Button(frame, text="Play", command=self.play_pause)
+        self.play_pause_btn.pack()
+
+        self.skip_plus_5sec = tk.Button(frame, text="Skip -5 sec", command=lambda: self.skip(-5))
+        self.skip_plus_5sec.pack(side="left")
+
+        self.start_time = tk.Label(frame, text=str(datetime.timedelta(seconds=0)))
+        self.start_time.pack(side="left")
+
+        self.progress_slider = tk.Scale(frame, from_=0, to=0, orient="horizontal")
+        self.progress_slider.bind("<ButtonRelease-1>", self.seek)
+        self.progress_slider.pack(side="left", fill="x", expand=True)
+
+        self.end_time = tk.Label(frame, text=str(datetime.timedelta(seconds=0)))
+        self.end_time.pack(side="left")
+
+        self.vid_player.bind("<<Duration>>", self.update_duration)
+        self.vid_player.bind("<<SecondChanged>>", self.update_scale)
+        self.vid_player.bind("<<Ended>>", self.video_ended )
+
+        self.skip_plus_5sec = tk.Button(frame, text="Skip +5 sec", command=lambda: self.skip(5))
+        self.skip_plus_5sec.pack(side="left")
+
+        self.load_video(path)
+        self.play_pause()
+
+    def update_duration(self, event):
+        """ updates the duration after finding the duration """
+        duration = self.vid_player.video_info()["duration"]
+        self.end_time["text"] = str(datetime.timedelta(seconds=duration))
+        self.progress_slider["to"] = duration
+
+
+    def update_scale(self, event):
+        """ updates the scale value """
+        self.progress_slider.set(self.vid_player.current_duration())
+
+
+    def load_video(self, file_path):
+        """ loads the video """
+        if file_path:
+            self.vid_player.load(file_path)
+
+            self.progress_slider.config(to=0, from_=0)
+            self.play_pause_btn["text"] = "Play"
+            self.progress_slider.set(0)
+
+
+    def seek(self, event=None):
+        """ used to seek a specific timeframe """
+        self.vid_player.seek(int(self.progress_slider.get()))
+
+
+    def skip(self, value: int):
+        """ skip seconds """
+        self.vid_player.seek(int(self.progress_slider.get())+value)
+        self.progress_slider.set(self.progress_slider.get() + value)
+
+
+    def play_pause(self):
+        """ pauses and plays """
+        if self.vid_player.is_paused():
+            self.vid_player.play()
+            self.play_pause_btn["text"] = "Pause"
+        else:
+            self.vid_player.pause()
+            self.play_pause_btn["text"] = "Play"
+
+
+    def video_ended(self, event):
+        """ handle video ended """
+        self.progress_slider.set(self.progress_slider["to"])
+        self.play_pause_btn["text"] = "Play"
+        self.progress_slider.set(0)
